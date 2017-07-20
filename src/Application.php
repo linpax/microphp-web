405976ad23ab806851351214bf0509b80721b782
Zcasper <testuser@mail.linpax.org>
2017-07-20 21:18:42 +0700

    init

---
+++ src/Application.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @link https://github.com/linpax/microphp-web
+ * @copyright Copyright (c) 2017 Linpax
+ * @license https://github.com/linpax/microphp-web/blob/master/LICENSE
+ */
+
+namespace Micro\Web;
+
+use Micro\Base\Kernel;
+
+
+/**
+ * Class Application
+ *
+ * @package Micro\Web
+ */
+class Application implements \Micro\Base\Application
+{
+    public function __construct(Kernel $kernel)
+    {
+    }
+
+    public function handle()
+    {
+        // TODO: Implement handle() method.
+    }
+}
---
+++ src/HMvcResolver.php
@@ -0,0 +1,252 @@
+<?php
+
+namespace Micro\Web;
+
+use Micro\Base\Exception;
+
+use Micro\Base\KernelInjector;
+use Micro\Base\ResolverInterface;
+use Micro\Base\RequestInjector;
+use Psr\Http\Message\ServerRequestInterface;
+
+class HMvcResolver implements ResolverInterface
+{
+    /** @var string $uri converted URL */
+    protected $uri;
+
+    /** @var string $extensions Extensions in request */
+    private $extensions;
+    /** @var string $modules Modules in request */
+    private $modules;
+    /** @var string $controller IController to run */
+    private $controller;
+    /** @var string $action Action to run */
+    private $action;
+
+
+    /**
+     * Get instance application
+     *
+     * @access public
+     *
+     * @return IController
+     * @throws Exception
+     */
+    public function getApp()
+    {
+        /** @var ServerRequestInterface $request */
+        $request = (new RequestInjector)->build();
+        /** @var array $rawQuery */
+        $rawQuery = $request->getQueryParams();
+
+        $query = !empty($rawQuery['r']) ? $rawQuery['r'] : '/default';
+        $query = (substr($query, -1) === '/') ? substr($query, 0, -1) : $query;
+
+        $this->uri = (new RouterInjector)->build()->parse($query, $request->getMethod());
+
+        $this->initialize();
+
+        /** @var string $cls */
+        $cls = $this->getCalculatePath();
+
+        if (!class_exists($cls) || !is_subclass_of($cls, '\Micro\Mvc\Controllers\IController')) {
+            throw new Exception('Controller '.$cls.' not found or not a valid');
+        }
+
+        return new $cls($this->getModules());
+    }
+
+    /**
+     * Initialize request object
+     *
+     * @access public
+     *
+     * @return void
+     * @throws Exception
+     */
+    protected function initialize()
+    {
+        $key = strpos($this->uri, '?');
+        $params = $key ? substr($this->uri, $key + 2) : null;
+        $uriBlocks = explode('/', substr($this->uri, 0, $key ?: strlen($this->uri)));
+
+        if (0 === strpos($this->uri, '/')) {
+            array_shift($uriBlocks);
+        }
+
+        $this->prepareExtensions($uriBlocks);
+        $this->prepareModules($uriBlocks);
+        $this->prepareController($uriBlocks);
+        $this->prepareAction($uriBlocks);
+
+        if ($params) {
+            $paramBlocks = explode('&', $params);
+            $query = (new RequestInjector)->build()->getQueryParams();
+
+            foreach ($paramBlocks AS $param) {
+                $val = explode('=', $param);
+
+                $query[$val[0]] = $val[1];
+            }
+
+            // replace request
+            $request = (new RequestInjector)->build();
+            (new RequestInjector)->addRequirement('request', $request->withQueryParams(
+                array_replace_recursive($request->getQueryParams(), $query)
+            ));
+        }
+    }
+
+    /**
+     * Prepare extensions
+     *
+     * @access private
+     *
+     * @param array $uriBlocks uri blocks from URL
+     *
+     * @return void
+     * @throws Exception
+     */
+    protected function prepareExtensions(&$uriBlocks)
+    {
+        foreach ($uriBlocks as $i => $block) {
+            if (file_exists((new KernelInjector)->build()->getAppDir().$this->extensions.'/extensions/'.$block)) {
+                $this->extensions .= '/Extensions/'.ucfirst($block);
+
+                unset($uriBlocks[$i]);
+            } else {
+                break;
+            }
+        }
+
+        $this->extensions = str_replace('/', '\\', $this->extensions);
+    }
+
+    /**
+     * Prepare modules
+     *
+     * @access private
+     *
+     * @global      Micro
+     *
+     * @param array $uriBlocks uri blocks from URL
+     *
+     * @return void
+     * @throws Exception
+     */
+    protected function prepareModules(&$uriBlocks)
+    {
+        $path = (new KernelInjector)->build()->getAppDir().($this->extensions ?: '');
+
+        foreach ($uriBlocks as $i => $block) {
+            if ($block && file_exists($path.strtolower($this->modules).'/modules/'.$block)) {
+                $this->modules .= '/Modules/'.ucfirst($block);
+
+                unset($uriBlocks[$i]);
+            } else {
+                break;
+            }
+        }
+
+        $this->modules = str_replace('/', '\\', $this->modules);
+    }
+
+    /**
+     * Prepare controller
+     *
+     * @access private
+     *
+     * @param array $uriBlocks uri blocks from URL
+     *
+     * @return void
+     * @throws Exception
+     */
+    protected function prepareController(&$uriBlocks)
+    {
+        $path = (new KernelInjector)->build()->getAppDir().($this->extensions ?: '').strtolower($this->modules ?: '');
+        $str = array_shift($uriBlocks);
+
+        if (file_exists(str_replace('\\', '/', $path.'/controllers/'.ucfirst($str).'Controller.php'))) {
+            $this->controller = $str;
+        } else {
+            $this->controller = 'default';
+
+            array_unshift($uriBlocks, $str);
+        }
+    }
+
+    /**
+     * Prepare action
+     *
+     * @access private
+     *
+     * @param array $uriBlocks uri blocks from URL
+     *
+     * @return void
+     */
+    protected function prepareAction(&$uriBlocks)
+    {
+        $this->action = array_shift($uriBlocks) ?: 'index';
+    }
+
+
+    /**
+     * Get calculate path to controller
+     *
+     * @access public
+     *
+     * @return string
+     */
+    public function getCalculatePath()
+    {
+        return '\\App'.$this->getExtensions().$this->getModules().'\\Controllers\\'.$this->getController();
+    }
+
+    /**
+     * Get extensions from request
+     *
+     * @access public
+     *
+     * @return string
+     */
+    public function getExtensions()
+    {
+        return $this->extensions;
+    }
+
+    /**
+     * Get modules from request
+     *
+     * @access public
+     *
+     * @return string
+     */
+    public function getModules()
+    {
+        return $this->modules;
+    }
+
+    /**
+     * Get controller from request
+     *
+     * @access public
+     *
+     * @return string
+     */
+    public function getController()
+    {
+        return ucfirst($this->controller).'Controller';
+    }
+
+    /**
+     * Get action from request
+     *
+     * @access public
+     *
+     * @return string
+     */
+    public function getAction()
+    {
+        return $this->action;
+    }
+}
